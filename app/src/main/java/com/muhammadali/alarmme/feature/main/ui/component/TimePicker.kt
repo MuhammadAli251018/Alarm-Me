dbd20f4e8816a1ce5b7fdabdb06a503c6e77eecf
MuhammadAli251018 <muhammadali251018@gmail.com>
2023-07-21 09:16:09 +0300

    fix: Fix visibility state of dialogs
    -bug -> time & date dialogs don't appear when set it's visibility to true
    Changed the visibility of the dialog to UseCaseState instead of boolean

--- app/src/main/java/com/muhammadali/alarmme/feature/main/ui/component/TimePicker.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/ui/component/TimePicker.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.DialogProperties
 import com.maxkeppeker.sheets.core.models.base.UseCaseState
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
 import com.maxkeppeler.sheets.clock.ClockDialog
 import com.maxkeppeler.sheets.clock.models.ClockSelection
 import com.muhammadali.alarmme.common.ui.theme.AlarmMeTheme
@@ -15,13 +16,10 @@ import com.muhammadali.alarmme.common.ui.theme.AlarmMeTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TimePicker(
-    isVisible: Boolean = true,
+    state: UseCaseState = rememberUseCaseState(true),
     onPositiveClick: (Int, Int) -> Unit = {_, _ ->},
     onNegativeClick: () -> Unit = {}
 ) {
-
-    // used that approach when you don't provide close or dismiss request
-    val state by remember { derivedStateOf { UseCaseState(isVisible) }}
 
     ClockDialog(state = state,
         selection = ClockSelection.HoursMinutes
--- app/src/main/java/com/muhammadali/alarmme/feature/main/ui/screen/util/MainScreenAction.kt
+++
@@ -1,7 +0,0 @@
-package com.muhammadali.alarmme.feature.main.ui.screen.util
-
-data class MainScreenAction(
-    val onAddBtnClick: () -> Unit,
-    val onItemClick: (index: Int) -> Unit,
-    val onItemSwitchBtnClick: (index: Int, isScheduled: Boolean) -> Unit
-)
