a9594be052015ca0040c4aab59b8b89e351bdc96
MuhammadAli251018 <muhammadali251018@gmail.com>
2023-07-21 19:04:38 +0300

    temp: Remove feature Snooze temporarily
    Remove feature Snooze temporarily because of bugs in Snooze Picker

--- app/src/main/java/com/muhammadali/alarmme/feature/main/ui/component/SnoozeModePicker.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/ui/component/SnoozeModePicker.kt
@@ -1,11 +1,8 @@
 package com.muhammadali.alarmme.feature.main.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,10 +10,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,28 +22,30 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.muhammadali.alarmme.common.ui.theme.AlarmMeTheme
-import com.muhammadali.alarmme.feature.main.ui.component.util.SnoozeMode
 import com.muhammadali.alarmme.feature.main.ui.component.util.SnoozeRepeat
+import com.muhammadali.alarmme.feature.main.ui.component.util.SnoozeState
 import com.muhammadali.alarmme.feature.main.ui.component.util.getAllSnoozeRepeat
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SnoozeModePicker(
-    showDialog: Boolean = true,
+    showDialog: Boolean,
     repeat: Array<SnoozeRepeat> = getAllSnoozeRepeat(),
-    chosenModeInit: SnoozeMode = SnoozeMode(SnoozeRepeat.FiveTimes, -1),
-    periods: Array<Int> = arrayOf(3, 5, -1),
-    onChooseMode: (SnoozeMode?) -> Unit ={},
-    onClosePicker: () -> Unit = {}
+    chosenStateInit: SnoozeState,
+    periods: Array<Int>,
+    onChooseMode: (SnoozeState) -> Unit,
+    onClosePicker: () -> Unit
 ) {
     if(showDialog) {
+        var chosenState by remember { mutableStateOf(chosenStateInit) }
 
-        var chosenMode by remember { mutableStateOf(chosenModeInit) }
         Dialog(
             onDismissRequest = {},
             properties = DialogProperties(
@@ -67,32 +67,90 @@ fun SnoozeModePicker(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    ChooseColumn(
-                        data = periods.map {"$it minutes"}.toTypedArray(),
-                        chosenIndex = periods.indexOf(chosenMode.period)
+                    //val snoozeOn by remember { mutableStateOf(chosenModeInit) }
+
+                    Card (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                        onClick = {
+                            chosenState = if (chosenState == SnoozeState.SnoozeOff)
+                                SnoozeState.SnoozeMode(SnoozeRepeat.FiveTimes, 3)
+                             else
+                                 SnoozeState.SnoozeOff
+                        }
                     ) {
-                        chosenMode = SnoozeMode(chosenMode.repeat, it)
-                    }
+                        Column (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
 
-                    Divider(Modifier.fillMaxWidth(), thickness = 1.dp)
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    ChooseColumn(
-                        data = repeat.map { it.content }.toTypedArray(),
-                        chosenIndex = repeat.indexOf(chosenMode.repeat)
-                    ) {
-                        chosenMode = SnoozeMode(chosenMode.repeat, it)
-                    }
-
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Button(onClick = onClosePicker) {
-                        Text(text = "Save", fontSize = 20.sp, color = Color.White)
+                            Text(
+                                text = if(chosenState != SnoozeState.SnoozeOff) "Snooze is On" else "Snooze is Off",
+                                fontSize = 20.sp,
+                                color = Color.White
+                            )
+                        }
                     }
 
                     Spacer(modifier = Modifier.height(20.dp))
+
+                    if (chosenState != SnoozeState.SnoozeOff) {
+
+                        var chosenMode: SnoozeState.SnoozeMode by remember { mutableStateOf(chosenStateInit as SnoozeState.SnoozeMode) }
+
+                        Text(
+                            text = "Snooze Repeat",
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.primary,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        ChooseColumn(
+                            data = periods.map { "$it minutes" }.toTypedArray(),
+                            chosenIndex = periods.indexOf(chosenMode.period)
+                        ) {
+                            chosenMode = SnoozeState.SnoozeMode(chosenMode.repeat, it)
+                            onChooseMode(chosenMode)
+                        }
+
+                        Divider(Modifier.fillMaxWidth(), thickness = 1.dp)
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = "Snooze Time",
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.primary,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        ChooseColumn(
+                            data = repeat.map { it.content }.toTypedArray(),
+                            chosenIndex = repeat.indexOf(chosenMode.repeat)
+                        ) {
+                            chosenMode = SnoozeState.SnoozeMode(chosenMode.repeat, it)
+                            onChooseMode(chosenMode)
+                        }
+
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Button(onClick = onClosePicker) {
+                            Text(text = "Save", fontSize = 20.sp, color = Color.White)
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                    }
                 }
             }
         }
@@ -102,7 +160,20 @@ fun SnoozeModePicker(
 @Preview(showBackground = true)
 @Composable
 fun PreviewSnoozeModePicker() {
+
+    val showDialog = true
+    val chosenModeInit = SnoozeState.SnoozeMode(SnoozeRepeat.FiveTimes, -1)
+    val periods: Array<Int> = arrayOf(3, 5, -1)
+    val onChooseMode: (SnoozeState) -> Unit ={}
+    val onClosePicker: () -> Unit = {}
+
     AlarmMeTheme {
-        SnoozeModePicker()
+        SnoozeModePicker(
+            showDialog = showDialog,
+            chosenStateInit = chosenModeInit,
+            periods = periods,
+            onChooseMode = onChooseMode,
+            onClosePicker = onClosePicker
+        )
     }
 }
