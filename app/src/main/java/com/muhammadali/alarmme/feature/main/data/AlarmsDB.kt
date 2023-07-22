24bf6a954a580780247def4eea926cd2f263ce0b
MuhammadAli251018 <muhammadali251018@gmail.com>
2023-07-22 09:06:18 +0300

    fix: Add inteties & version to alarmsdb class

--- app/src/main/java/com/muhammadali/alarmme/feature/main/data/AlarmsDB.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/data/AlarmsDB.kt
@@ -3,7 +3,7 @@ package com.muhammadali.alarmme.feature.main.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database
+@Database(entities = [Alarm::class], version = 1)
 abstract class AlarmsDB : RoomDatabase() {
     abstract fun alarmsDao(): AlarmsDao
 }
