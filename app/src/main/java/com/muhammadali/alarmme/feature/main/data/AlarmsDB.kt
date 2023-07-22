469bdd0383a60f1d36bd8f1f04d8dad25d18527e
MuhammadAli251018 <muhammadali251018@gmail.com>
2023-07-22 09:04:19 +0300

    fix: Add database annotation to AlarmsDB

--- app/src/main/java/com/muhammadali/alarmme/feature/main/data/AlarmsDB.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/data/AlarmsDB.kt
@@ -1,7 +1,9 @@
 package com.muhammadali.alarmme.feature.main.data
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database
 abstract class AlarmsDB : RoomDatabase() {
     abstract fun alarmsDao(): AlarmsDao
 }
