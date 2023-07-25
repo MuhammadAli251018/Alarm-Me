78fbd77f81a1ae01978b7044770805326186cd06
MuhammadAli251018 <muhammadali251018@gmail.com>
2023-07-25 18:27:14 +0300

    typo: Fix typo in exception message

--- app/src/main/java/com/muhammadali/alarmme/common/AlarmExceptions.kt
+++ app/src/main/java/com/muhammadali/alarmme/common/AlarmExceptions.kt
@@ -1,5 +1,5 @@
 package com.muhammadali.alarmme.common
 
 sealed class AlarmExceptions(message: String) : Exception(message) {
-    object NotificationPermissionNotGranted : AlarmExceptions("Notifications persmission is not granted")
+    object NotificationPermissionNotGranted : AlarmExceptions("Notifications permission is not granted")
 }
