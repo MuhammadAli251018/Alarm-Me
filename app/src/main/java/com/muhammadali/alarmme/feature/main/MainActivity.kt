5b42a72665cbd1e371ad59f58df52dd934dc3279
muhammad ali <muhammad251018@gmail.com>
2024-07-17 12:59:45 +0300

    feat: Add main screen & it's state & action
    -Added the main screen & it's commponent completly
    -Edited the package structure of the application & location of main activity

--- app/src/main/java/com/muhammadali/alarmme/feature/main/MainActivity.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.muhammadali.feature.main
+package com.muhammadali.alarmme.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.muhammadali.feature.main.ui.theme.AlarmMeTheme
+import com.muhammadali.alarmme.common.ui.theme.AlarmMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/ui/component/AlarmItemState.kt
@@ -0,0 +1,33 @@
+package com.muhammadali.alarmme.feature.main.ui.component
+
+import androidx.compose.ui.text.AnnotatedString
+
+data class AlarmItemState(
+    val alarmTitle: String,
+    val alarmTime: AnnotatedString,
+    val alarmRepeat: BooleanArray,
+    val isScheduled: Boolean,
+    val isEnabled: Boolean
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AlarmItemState
+
+        if (alarmTitle != other.alarmTitle) return false
+        if (alarmTime != other.alarmTime) return false
+        if (!alarmRepeat.contentEquals(other.alarmRepeat)) return false
+        if (isScheduled != other.isScheduled) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = alarmTitle.hashCode()
+        result = 31 * result + alarmTime.hashCode()
+        result = 31 * result + alarmRepeat.contentHashCode()
+        result = 31 * result + isScheduled.hashCode()
+        return result
+    }
+}
