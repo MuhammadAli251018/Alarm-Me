627aff5cd54e1d8a96e29d93f06566a4267b9152
muhammad ali <muhammad251018@gmail.com>
2024-07-17 05:20:42 +0300

    ui: Add the colors & themes
    Added the main colors to themes, only imlemented the dark theme
    & made the light theme the same as the dark

--- app/src/main/java/com/muhammadali/alarmme/ui/theme/Color.kt
+++ app/src/main/java/com/muhammadali/alarmme/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// project colors
+
+val Calypso = Color(0xFF296B80)
+val Tuna = Color(0xFF34344A)
+val Shark = Color(0xFF1D1D23)
