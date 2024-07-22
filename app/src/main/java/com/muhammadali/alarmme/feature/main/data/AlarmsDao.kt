88dd9de3357daffebde3550ed60dafe12caf3ddb
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-07-22 08:41:12 +0300

    feat: Add Alarm Database package
    Add Alarm Database with room where the dao consists CRUD operations

---
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/data/AlarmsDao.kt
@@ -0,0 +1,27 @@
+package com.muhammadali.alarmme.feature.main.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlarmsDao {
+
+    @Query("SELECT * FROM alarms")
+    fun getAllAlarms(): Flow<List<Alarm>>
+
+    /** the elements are put in ASC order */
+    @Query("SELECT * FROM alarms WHERE scheduled = 1 ORDER BY `index` ASC")
+    fun getScheduledAlarm(): Flow<List<Alarm>>
+
+    /**
+     * elements must have the same id
+     * */
+    @Upsert
+    suspend fun insertOrUpdateAlarm(alarm: Alarm)
+
+    @Delete
+    suspend fun deleteAlarm(alarm: Alarm)
+}
