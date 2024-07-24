ba37592d00961181f175e9fbc3c63db6d23dbcd5
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-07-24 13:02:31 +0300

    garbage commit

--- app/src/main/java/com/muhammadali/alarmme/feature/main/MainActivity.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.muhammadali.alarmme.common.ui.theme.AlarmMeTheme
+import com.muhammadali.alarmme.feature.main.domain.AlarmScheduler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
