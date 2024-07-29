0f43c6bcfa0643b3a63d1dfafdf2dbf6bbbd6292
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-07-29 14:40:37 +0300

    fix: Add excat alarms availble check

--- app/src/main/java/com/muhammadali/alarmme/feature/main/domain/AlarmSchedulerImp.kt
+++ app/src/main/java/com/muhammadali/alarmme/feature/main/domain/AlarmSchedulerImp.kt
@@ -5,6 +5,8 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import com.muhammadali.alarmme.common.AlarmConstants
 import javax.inject.Inject
 
@@ -38,7 +40,24 @@ class AlarmSchedulerImp @Inject constructor(
             intent,
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+        //todo log for testing
+        val is31OrAbove = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+        Log.d("AlarmScheduler", "is31OrAbove: $is31OrAbove")
+
+        if (is31OrAbove) {
+            //todo log for testing
+            val canSchedule = alarmManager.canScheduleExactAlarms()
+            Log.d("AlarmScheduler", "canSchedule: $canSchedule")
+
+            if (canSchedule)
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+            else {
+            }
+        }    // handle
+        else {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+        }
+            //handle
     }
 
     override fun cancelAlarm( context: Context, receiver: Class<out BroadcastReceiver>, alarmId: Int) {
